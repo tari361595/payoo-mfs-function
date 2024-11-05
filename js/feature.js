c314f0bc80990715d66c813d126826a203d85043
tari361595 <cmtariqulislam1994@gmail.com>
2024-11-06 04:10:22 +0900

    done feature section

---
+++ js/feature.js
@@ -0,0 +1,14 @@
+// console.log('added feature file');
+document.getElementById('show-add-money').addEventListener('click',function(){
+  
+    showSectionById('add-money-form');
+});
+
+document.getElementById('show-cash-out').addEventListener('click',function(){
+    showSectionById('cash-out-form')
+})
+document.getElementById('show-transaction').addEventListener('click',function(){
+    showSectionById('transaction-section');
+})
+
+
--- js/utilities.js
+++ js/utilities.js
@@ -15,3 +15,12 @@ function getTextFieldValueById(id){
     const textNumber = parseFloat(textValue);
     return textNumber;
 }
+
+function showSectionById(id){
+    document.getElementById('add-money-form').classList.add('hidden')
+    document.getElementById('cash-out-form').classList.add('hidden')
+    document.getElementById('transaction-section').classList.add('hidden')
+
+    document.getElementById(id).classList.remove('hidden')
+
+}
